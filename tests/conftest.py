8e41dd4da3ed8d065a511e368f5cfb637336bfc7
Ramin Safarov <silent1962@gmail.com>
2025-04-17 17:09:32 +0300

    lesson-11: fix 6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,21 @@
 import pytest
+import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
 from utils import attach
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope='function')
 def browser_config(request):
@@ -20,7 +31,7 @@ def browser_config(request):
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"http://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options
     )
 
