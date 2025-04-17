503e00523392844d83b68f2cbca3adf964c263cc
Ramin Safarov <silent1962@gmail.com>
2025-04-17 14:43:31 +0300

    lesson-11: Jenkins settings 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,27 @@
 import pytest
-from selene import browser
+
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
-
-# Настройка размера окна браузера
-@pytest.fixture()
-def browser_config():
+@pytest.fixture(scope='function')
+def browser_config(request):
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('window-size=1920,1080')
-    browser.config.driver_options = options
-    browser.config.base_url = "https://demoqa.com"
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    browser.quit()
