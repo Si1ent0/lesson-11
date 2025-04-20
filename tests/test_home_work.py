9475f9f9e0d3d7e325bda6378a09129b137f9b1b
Ramin Safarov <silent1962@gmail.com>
2025-04-20 15:48:34 +0300

    lesson-11: fix 17

--- tests/test_home_work.py
+++ tests/test_home_work.py
@@ -1,8 +1,17 @@
 import allure
 
+from allure_commons.types import Severity
 from lesson_11_tests.Application import app
 from lesson_11_tests.data import users
 
+
+@allure.feature("Registry")
+@allure.epic("Student reg")
+@allure.story("Stud reg page")
+@allure.label("owner", "RS")
+@allure.tag("smoke", "regression", "web", "reg")
+@allure.severity(Severity.CRITICAL)
+@allure.link("https://demoqa.com/automation-practice-form", name="stud reg")
 @allure.step("Отправка формы регистрации студента")
 def test_send_practice_form_(browser_config):
     app.new_reg.open_stud_reg_page()
